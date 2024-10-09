a279324d9f926a2f4f07008f51a327737aa67e34
Shaun Culver <shaun@glucode.com>
2024-10-09 12:54:10 +0200

    Restructure

--- AppleStoreDesignPatterns/AppleStoreComposite/Sources/Examples/SimpleExample/SimpleExample.swift
+++ AppleStoreDesignPatterns/AppleStoreComposite/Sources/Examples/SimpleExample/SimpleExample.swift
@@ -1,21 +1,23 @@
 import SwiftUI
 
 public struct SimpleExampleComposite {
-    public protocol Component: Hashable {
+    // Component
+    public protocol CatalogItem: Hashable {
         var id: String { get }
         var name: String { get }
     }
     
-    public struct Category: Component {
+    // Composite
+    public struct CatalogCategory: CatalogItem {
         public let id: String
         public let name: String
-        public var children: [any Component] = []
+        public var children: [any CatalogItem] = []
         
-        public mutating func addComponent(_ component: any Component) {
+        public mutating func addComponent(_ component: any CatalogItem) {
             children.append(component)
         }
         
-        public mutating func removeComponent(_ component: any Component) {
+        public mutating func removeComponent(_ component: any CatalogItem) {
             if let index = children.firstIndex(where: { $0.id == component.id }) {
                 children.remove(at: index)
             }
@@ -25,12 +27,13 @@ public struct SimpleExampleComposite {
             hasher.combine(id)
         }
         
-        public static func == (lhs: Category, rhs: Category) -> Bool {
+        public static func == (lhs: CatalogCategory, rhs: CatalogCategory) -> Bool {
             return lhs.id == rhs.id
         }
     }
     
-    public struct Product: Component {
+    // Leaf
+    public struct CatalogProduct: CatalogItem {
         public let id: String
         public let name: String
         public let description: String
@@ -40,22 +43,22 @@ public struct SimpleExampleComposite {
             hasher.combine(id)
         }
         
-        public static func == (lhs: Product, rhs: Product) -> Bool {
+        public static func == (lhs: CatalogProduct, rhs: CatalogProduct) -> Bool {
             return lhs.id == rhs.id
         }
     }
 }
 
 public class SimpleExampleViewModel: ObservableObject {
-    @Published public var rootComponent = SimpleExampleComposite.Category(id: "0", name: "Catalog")
+    @Published public var rootComponent = SimpleExampleComposite.CatalogCategory(id: "0", name: "Catalog")
     
     public func addCategory() {
-        var category = SimpleExampleComposite.Category(id: UUID().uuidString, name: "Category")
+        var category = SimpleExampleComposite.CatalogCategory(id: UUID().uuidString, name: "Category")
         rootComponent.addComponent(category)
     }
     
     public func addProduct() {
-        let product = SimpleExampleComposite.Product(
+        let product = SimpleExampleComposite.CatalogProduct(
             id: UUID().uuidString,
             name: "Apple Vision Pro",
             description: "Seamlessly blends digital content with your physical space.",
@@ -81,7 +84,7 @@ public struct SimpleExampleView: View {
                 List {
                     Section(header: Text("Categories")) {
                         ForEach(viewModel.rootComponent.children.compactMap {
-                            $0 as? SimpleExampleComposite.Category
+                            $0 as? SimpleExampleComposite.CatalogCategory
                         }, id: \.id) { category in
                             HStack {
                                 Image(systemName: "cube")
@@ -97,7 +100,7 @@ public struct SimpleExampleView: View {
                     
                     Section(header: Text("Products")) {
                         ForEach(viewModel.rootComponent.children.compactMap {
-                            $0 as? SimpleExampleComposite.Product
+                            $0 as? SimpleExampleComposite.CatalogProduct
                         }, id: \.id) { product in
                             HStack {
                                 Image(systemName: "visionpro")
